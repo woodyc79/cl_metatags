336e534e9c07d6409a7f7f2a6c24d1ec64c54d1a
Sven Wappler <info@wapplersystems.de>
2013-03-13 13:46:35 +0100

    first commit

---
+++ ext_emconf.php
@@ -0,0 +1,48 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext "cl_metatags".
+#
+# Auto generated 30-07-2012 00:40
+#
+# Manual updates:
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'Extended meta tags',
+	'description' => 'Extended number of meta tags editable in Constant Editor. Includes geo tags and facebook open graph meta tags.',
+	'category' => 'fe',
+	'shy' => 0,
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'loadOrder' => '',
+	'module' => '',
+	'state' => 'stable',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 1,
+	'lockType' => '',
+	'author' => 'Sven Wappler',
+	'author_email' => 'typo3@wapplersystems.de',
+	'author_company' => 'WapplerSystems',
+	'CGLcompliance' => '',
+	'CGLcompliance_note' => '',
+	'version' => '2.0.1',
+	'constraints' => array(
+		'depends' => array(
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:4:{s:12:"ext_icon.gif";s:4:"e199";s:28:"ext_typoscript_constants.txt";s:4:"63b9";s:24:"ext_typoscript_setup.txt";s:4:"6f70";s:14:"doc/manual.sxw";s:4:"f594";}',
+	'suggests' => array(
+	),
+);
+
+?>
